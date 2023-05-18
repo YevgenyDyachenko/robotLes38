5c93aa48d0e6fa91d2ecaa59ed0334bf4ed274b3
”YevgenyDyachenko” <yevgeny.dyachenko@gmail.com>
2023-05-18 23:44:42 +0300

    homework1

---
+++ src/main/java/com/ua/robotles39/RobotLes39Application.java
@@ -0,0 +1,13 @@
+package com.ua.robotles39;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotLes39Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotLes39Application.class, args);
+    }
+
+}
