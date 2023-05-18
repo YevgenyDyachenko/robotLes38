3e22962a1dec3b086a54ed95814f00b4e0f6c888
”YevgenyDyachenko” <yevgeny.dyachenko@gmail.com>
2023-05-18 23:41:51 +0300

    Initial commit

---
+++ src/main/java/com/ua/robotles38/RobotLes38Application.java
@@ -0,0 +1,13 @@
+package com.ua.robotles38;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotLes38Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotLes38Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ua/robotles38/RobotLes38ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ua.robotles38;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotLes38ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
